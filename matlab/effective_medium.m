7670ed9c287b70f690e3ebfe3319732889f72c1a
bernsen <stevenbernsen@gmail.com>
2018-11-13 13:16:20 -0500

    More robust comments

--- matlab/effective_medium.m
+++ matlab/effective_medium.m
@@ -1,20 +1,21 @@
 % Compute the elastic effective medium of a polymonomineral
-
+tic
 % Wipe the slate clean
 clc;
 
-% Define some necessary variables
+%% Define some necessary variables to create a synthetic crystal fabric
 phi = 0*pi/180;    % z1 rotation
 theta = 0*pi/180;  % x-rotation
 psi = 0*pi/180;    % z2-rotation
 
 % standard deviations of the rotation angles 
-sdphi = 20*pi/180;     
-sdtheta = 20*pi/180; 
-sdpsi = 20*pi/180;
+sdphi = 40*pi/180;     
+sdtheta = 25*pi/180; 
+sdpsi = 40*pi/180;
 
-npts = 1000;
-volume_fraction = ones(2*npts, 1)./(2*npts);
+% define how many points to use for the homogenization and each crystals contribution to the volume
+npts = 100000;
+volume_fraction = ones(npts, 1)./(npts);
 
 % Define the stiffness tensor 
 C = [   148.3183 81.2892 68.1977 0 0 0;... 
@@ -25,24 +26,34 @@ C = [   148.3183 81.2892 68.1977 0 0 0;...
         0 0 0 0 0 33.5145];
 
 
-%% 
+%% Synthesize the set of euler angles for a z-x-z
+rnd1 = rand(npts,1);
+rnd1 = -(1-rnd1) + rnd1;
 
-phi = [phi + rand(npts, 1).*sdphi; phi - rand(npts, 1).*sdphi];
-theta = [theta + rand(npts, 1).*sdtheta; theta - rand(npts, 1).*sdtheta];
-psi = [psi + rand(npts, 1).*sdpsi; psi - rand(npts, 1).*sdpsi];
+rnd2 = rand(npts,1);
+rnd2 = -(1-rnd2) + rnd2;
+
+rnd3 = rand(npts,1);
+rnd3 = -(1-rnd3) + rnd3;
+
+phi = phi + rnd1.*sdphi;
+theta = theta + rnd2.*sdtheta;
+psi = psi + rnd3.*sdpsi;
 
 
-euler_set = zeros(2*npts, 3);
+euler_set = zeros(npts, 3);
 euler_set(:,1) = phi;
 euler_set(:,2) = theta;
 euler_set(:,3) = psi;
 
 
+%% Homogenize
 
 [V, R, H] = vrh_homogenization(C, euler_set, volume_fraction);
 
+% The material is now orthorhombic so we only need the upper left 3-by-3 and the lower right diagonal
 V = V.*(C ~=0);
 R = R.*(C ~=0);
 H = H.*(C ~=0);
+toc
 
-% Confess your sins then go do it all over again
--- matlab/vrh_homogenization.m
+++ matlab/vrh_homogenization.m
@@ -5,9 +5,10 @@ function [V, R, H] = vrh_homogenization(C, euler_set, fraction)
 % INPUT
 %   C - the 6-by-6 stiffness matrix 
 %   euler_set - the m-by-3 set of euler angles (phi, theta, psi)
+%   fraction - the m-by-1 set of volume fractions for each crystal
 %
 % OUTPUT
-%
+%	V,R,H - the 6-by-6 matrices for each homogenization
 %--------------------------------------------------------------------------
 
 m = length(euler_set(:,1) );
@@ -20,17 +21,21 @@ V = zeros(6, 6);
 R = V;
 H = V;
 
+
 for i = 1:m
+   % Get the rotation matrix
    rot = euler_rotation(euler_set(i,1), euler_set(i,2), euler_set(i,3) );
+   % Plug the rotation matrix in to get the bond matrix
    M = bond(rot);
    N = M';
-   V = V + fraction(i).*( M*C*M');
-   R = R + ( fraction(i) ).*(N*S*N');
+   V = V + fraction(i).*( M*C*M'); % Voigt
+   R = R + ( fraction(i) ).*(N*S*N'); % Reuss
    
 end
 
-R = inv(R);
-H = (V + R)./2;
+% 
+R = inv(R); 
+H = (V + R)./2; % Hill average
 
 
 end
@@ -51,12 +56,12 @@ B = [cos(psi) -sin(psi) 0; sin(psi) cos(psi) 0; 0 0 1];
 
 R = D*B*C;
 
-
 end
 
-function [M] = bond(R)
-%BOND computes the 6-by-6 bond rotation matrix to be applied to the  
 
+function [M] = bond(R)
+% BOND computes the 6-by-6 bond rotation matrix to be applied to the  
+% stiffness or compliance matrices
 
 M = [  R(1,1) R(1,2) R(1,3) ...
                 2*R(1,2)*R(1,3)	2*R(1,3)*R(1,1) 2*R(1,1)*R(1,2);...
@@ -77,7 +82,7 @@ M = [  R(1,1) R(1,2) R(1,3) ...
                 R(1,3)*R(2,1) + R(1,1)*R(2,3)...
                 R(1,1)*R(2,2) + R(1,2)*R(2,1) ];
 
-
+          
 end
 
 
